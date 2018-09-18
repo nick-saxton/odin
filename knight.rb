c60b6ef235cf1ed0a3214395b0ea0fe39d864201
Nick Saxton <nick.saxton@gmail.com>
2018-09-18 10:38:52 -0400

    Working Knight's Travails

--- knight.rb
+++ knight.rb
@@ -13,6 +13,25 @@ class Graph
     build_graph
   end
 
+  def bfs(value, starting_vertex)
+    starting_vertex.distance = 0
+    queue = [starting_vertex]
+    while !queue.empty?
+      vertex = queue.shift
+
+      if value == vertex.value
+        return
+      end
+
+      vertex.neighbors.each do |neighbor|
+        if neighbor.distance.nil?
+          neighbor.distance = vertex.distance + 1
+          queue.push(neighbor)
+        end
+      end
+    end
+  end
+
   def build_graph
     # All of the possible jumps a knight can make
     possible_moves = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
@@ -34,6 +53,45 @@ class Graph
     end
   end
 
+  def knight_move(start_coordinates, end_coordinates)
+    start_vertex = @vertices[start_coordinates[0]][start_coordinates[1]]
+    end_vertex = @vertices[end_coordinates[0]][end_coordinates[1]]
+
+    # Calculate the distance from the start vertex for each other verte
+    bfs(end_coordinates, start_vertex)
+
+    if !end_vertex.distance.nil?
+      puts "You made it in #{@vertices[end_coordinates[0]][end_coordinates[1]].distance} move(s)! Here's your path: "
+      path = [end_vertex]
+      current_vertex = end_vertex
+      while current_vertex.distance > 0
+        closest_neighbor = nil
+        current_vertex.neighbors.each do |neighbor|
+          if (!neighbor.distance.nil? and (closest_neighbor.nil? or neighbor.distance < closest_neighbor.distance))
+          # if (closest_neighbor.nil? and !neighbor.distance.nil?) or (!neighbor.distance.nil? and neighbor.distance < closest_neighbor.distance)
+            closest_neighbor = neighbor
+          end
+        end
+        current_vertex = closest_neighbor
+        path.unshift(current_vertex)
+      end
+      path.each do |vertex|
+        puts vertex.value.to_s
+      end
+    end
+
+    # Reset the distances for the next search
+    reset_distances
+  end
+
+  def reset_distances
+    @vertices.each do |column|
+      column.each do |vertex|
+        vertex.distance = nil
+      end
+    end
+  end
+
   def to_s
     str = ""
     (0..7).each do |i|
@@ -43,30 +101,27 @@ class Graph
     end
     str
   end
-  
+
   class Vertex
-    attr_accessor :neighbors
-    attr_reader :column, :row
+    attr_accessor :neighbors, :distance
+    attr_reader :column, :row, :value
 
     def initialize(column, row)
       @column = column
       @row = row
+      @value = [column, row]
       @neighbors = []
+      @distance = nil
     end
 
     def to_s
-      str = "Vertex (#{@column}, #{@row}): "
-      @neighbors.each_with_index do |neighbor, index|
-        if index != 0
-          str += ", "
-        end
-        str += "(#{neighbor.column}, #{neighbor.row})"
-      end
-
-      str
+      "#{@value} - distance: #{@distance}"
     end
   end
 end
 
 graph = Graph.new
-puts graph
+graph.knight_move([0, 0], [1, 2])
+graph.knight_move([0, 0], [7, 7])
+graph.knight_move([3, 3], [4, 6])
+graph.knight_move([3, 3], [4, 3])
