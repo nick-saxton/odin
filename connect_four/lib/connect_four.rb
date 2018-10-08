2475ebc2bce4a14d0820113892727154f7e32243
Nick Saxton <nick.saxton@gmail.com>
2018-10-08 10:39:45 -0500

    Updated tests and game is in working condition

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -1,16 +1,8 @@
 class ConnectFour
     def initialize
         @columns = [[], [], [], [], [], [], []]
-        @player_1_marker = "\u26AA"
-        @player_2_marker = "\u26AB"
-    end
-
-    def check_for_win(row, column)
-        marker_to_check = @columns[row][column]
-
-        return true if check_for_vertical_win(row, column, marker_to_check)
-        return true if check_for_horizontal_win(row, column, marker_to_check)
-        return true if check_for_diagonal_win(row, column, marker_to_check)
+        @player_1_marker = "x"
+        @player_2_marker = "o"
     end
 
     def check_for_diagonal_win(row, column, marker_to_check)
@@ -18,20 +10,28 @@ class ConnectFour
         diagonal2_to_check = []
 
         (-3..3).each do |n|
-            if !@columns[row + n][column + n].nil?
-                diagonal1_to_check << @columns[row + n][column + n]
+            if !@columns[column + n][row + n].nil?
+                diagonal1_to_check << @columns[column + n][row + n]
             end
 
-            if n <= 0
-                if !@columns[row + n][column - n].nil?
-                    diagonal2_to_check << @columns[row + n][column - n]
-                end
-            else
-                if !@columns[row - n][column + n].nil?
-                    diagonal2_to_check << @columns[row - n][column + n]
-                end
+            if !@columns[column + n][row - n].nil?
+                diagonal2_to_check << @columns[column + n][row - n]
             end
         end
+
+        if diagonal1_to_check.length >=4
+            (diagonal1_to_check.length - 3).times do |n|
+                return true if diagonal1_to_check[n..(n + 3)].all? { |marker| marker == marker_to_check }
+            end
+        end
+
+        if diagonal2_to_check.length >= 4
+            (diagonal2_to_check.length - 3).times do |n|
+                return true if diagonal2_to_check[n..(n + 3)].all? { |marker| marker == marker_to_check }
+            end
+        end
+
+        false
     end
 
     def check_for_horizontal_win(row, column, marker_to_check)
@@ -57,6 +57,11 @@ class ConnectFour
         false
     end
 
+    def check_for_tie
+        return true if @columns.all? { |column| column.length == 6 }
+        false
+    end
+
     def check_for_vertical_win(row, column, marker_to_check)
         # Since this will be checked based on the last dropped
         # marker, we can rule out the first 3 dropped markers
@@ -71,6 +76,14 @@ class ConnectFour
         false
     end
 
+    def check_for_win(row, column)
+        marker_to_check = @columns[row][column]
+
+        return true if check_for_vertical_win(row, column, marker_to_check)
+        return true if check_for_horizontal_win(row, column, marker_to_check)
+        return true if check_for_diagonal_win(row, column, marker_to_check)
+    end
+
     def drop(marker, column)
         if column < 0 or column > 6
             raise BadColumnError
@@ -81,6 +94,62 @@ class ConnectFour
         end
 
         @columns[column] << marker
+    end
+
+    def game_loop
+        puts
+        puts "Let's play a game of Connect Four!"
+        puts
+        puts "Take turns picking a column number to drop your marker into"
+        puts "and the first to connect four in a row horizontally, vertically,"
+        puts "or diagonally wins!"
+        puts
+        render_grid
+
+        turn_counter = 0
+
+        while true do
+            begin
+                if turn_counter % 2 == 0
+                    current_marker = @player_1_marker
+                else
+                    current_marker = @player_2_marker
+                end
+
+                puts
+                print "Choose a column (1-7) or hit Ctrl-c to quit: "
+
+                column_selection = handle_input(gets.chomp)
+
+                drop(current_marker, column_selection)
+            rescue BadColumnError
+                puts "The column number you've provided is invalid. Please try again."
+                retry
+            rescue Interrupt
+                puts "Thanks for playing! Have a nice day."
+                break
+            end
+
+            if check_for_win(@columns[column_selection].length - 1, column_selection)
+                puts "#{} connected four in a row and wins!"
+                break
+            end
+
+            if check_for_tie
+                puts "It looks like it's ended in a tie! Thanks for playing."
+                break
+            end
+
+            puts
+            render_grid
+
+            turn_counter += 1
+        end
+    end
+
+    def handle_input input
+        raise BadColumnError if !(input =~ /^[1-7]$/)
+        input.to_i - 1
     end
 
     def render_grid
@@ -98,5 +167,5 @@ class ConnectFour
     end
 
     class BadColumnError < StandardError; end
-    class FullColumnError < StandardError; end
+    class FullColumnError < BadColumnError; end
 end
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -13,61 +13,95 @@ describe ConnectFour do
         end
     end
 
-    it "starts with \u26AA as player 1's marker" do
-        expect(game.instance_variable_get(:@player_1_marker)).to eql("\u26AA")
+    it "starts with x as player 1's marker" do
+        expect(game.instance_variable_get(:@player_1_marker)).to eql("x")
     end
 
-    it "starts with \u26AB as player 1's marker" do
-        expect(game.instance_variable_get(:@player_2_marker)).to eql("\u26AB")
+    it "starts with o as player 1's marker" do
+        expect(game.instance_variable_get(:@player_2_marker)).to eql("o")
     end
 
     context "#drop" do
         it "raises a BadColumnError when given a bad column" do
-            expect{ game.drop("\u26AA", -1) }.to raise_error(ConnectFour::BadColumnError)
+            expect{ game.drop("x", -1) }.to raise_error(ConnectFour::BadColumnError)
         end
 
         it "raises a FullColumnError when given a full column" do
-            game.instance_variable_get(:@columns)[0] = ["\u26AA", "\u26AB", "\u26AA", "\u26AB", "\u26AA", "\u26AB"]
-            expect{ game.drop("\u26AA", 0) }.to raise_error(ConnectFour::FullColumnError)
+            game.instance_variable_get(:@columns)[0] = ["x", "o", "x", "o", "x", "o"]
+            expect{ game.drop("x", 0) }.to raise_error(ConnectFour::FullColumnError)
         end
 
         it "works on an empty column" do
-            game.drop("\u26AA", 0)
-            expect(game.instance_variable_get(:@columns)[0].last).to eql("\u26AA")
+            game.drop("x", 0)
+            expect(game.instance_variable_get(:@columns)[0].last).to eql("x")
         end
 
         it "works on a non-empty column" do
-            game.instance_variable_get(:@columns)[0] << "\u26AA"
-            game.drop("\u26AB", 0)
-            expect(game.instance_variable_get(:@columns)[0]).to eql(["\u26AA", "\u26AB"])
+            game.instance_variable_get(:@columns)[0] << "x"
+            game.drop("o", 0)
+            expect(game.instance_variable_get(:@columns)[0]).to eql(["x", "o"])
         end
     end
 
     context "#check_for_vertical_win" do
         it "returns false when the given row is less than 3" do
-            expect(game.check_for_vertical_win(0, 0, "\u26AA")).to eql(false)
+            expect(game.check_for_vertical_win(0, 0, "x")).to eql(false)
         end
 
         it "returns true when the given row and column position is part of 4 in a row vertically" do
-            game.instance_variable_get(:@columns)[0] = ["\u26AA", "\u26AA", "\u26AA", "\u26AA"]
-            expect(game.check_for_vertical_win(3, 0, "\u26AA")).to eql(true)
+            game.instance_variable_get(:@columns)[0] = ["x", "x", "x", "x"]
+            expect(game.check_for_vertical_win(3, 0, "x")).to eql(true)
         end
 
         it "returns false when the given row and column position does not make 4 in a row vertically" do
-            game.instance_variable_get(:@columns)[0] = ["\u26AA", "\u26AA", "\u26AB", "\u26AA"]
-            expect(game.check_for_vertical_win(3, 0, "\u26AA")).to eql(false)
+            game.instance_variable_get(:@columns)[0] = ["x", "x", "o", "x"]
+            expect(game.check_for_vertical_win(3, 0, "x")).to eql(false)
         end
     end
 
     context "#check_for_horizontal_win" do
-        it "returns true when the given row and column position is part of a 4 in a row horizontally" do
-            game.instance_variable_set(:@columns, [["\u26AA"], ["\u26AA"], ["\u26AA"], ["\u26AA"], [], [], []])
-            expect(game.check_for_horizontal_win(0, 0, "\u26AA")).to eql(true)
+        it "returns true when the given row and column position is part of 4 in a row horizontally" do
+            game.instance_variable_set(:@columns, [["x"], ["x"], ["x"], ["x"], [], [], []])
+            expect(game.check_for_horizontal_win(0, 0, "x")).to eql(true)
         end
 
         it "returns false when the given row and column position does not make 4 in a row horizontally" do
-            game.instance_variable_set(:@columns, [["\u26AA"], ["\u26AB"], ["\u26AA"], ["\u26AA"], [], [], []])
-            expect(game.check_for_horizontal_win(0, 0, "\u26AA")).to eql(false)
+            game.instance_variable_set(:@columns, [["x"], ["o"], ["x"], ["x"], [], [], []])
+            expect(game.check_for_horizontal_win(0, 0, "x")).to eql(false)
+        end
+    end
+
+    context "#check_for_diagonal_win" do
+        it "returns true when the given row and column position is part of 4 in a row diagonally" do
+            game.instance_variable_set(:@columns, [["x"], ["o", "x"], ["x", "o", "x"], ["x", "o", "x", "x"], [], [], []])
+            expect(game.check_for_diagonal_win(2, 2, "x")).to eql(true)
+        end
+
+        it "returns false when the given row and column position is not part of 4 in a row diagonally" do
+            game.instance_variable_set(:@columns, [["x"], ["o", "x"], ["x", "o", "x"], ["x", "o", "x", "x"], [], [], []])
+            expect(game.check_for_diagonal_win(3, 1, "o")).to eql(false)
+        end
+    end
+
+    context "#check_for_tie" do
+        it "returns true if every space has been filled in the grid" do
+            game.instance_variable_set(:@columns, [["x", "x", "o", "o", "x", "x"], ["x", "x", "o", "o", "x", "x"], ["o", "o", "x", "x", "o", "o"], ["o", "o", "x", "x", "o", "o"], ["x", "x", "o", "o", "x", "x"], ["x", "x", "o", "o", "x", "x"], ["o", "o", "x", "x", "o", "o"]])
+            expect(game.check_for_tie).to eql(true)
+        end
+
+        it "returns false if there are still spaces to mark" do
+            game.instance_variable_set(:@columns, [["x", "x", "o", "o", "x"], ["x", "x", "o", "o", "x", "x"], ["o", "o", "x", "x", "o", "o"], ["o", "o", "x", "x", "o", "o"], ["x", "x", "o", "o", "x", "x"], ["x", "x", "o", "o", "x", "x"], ["o", "o", "x", "x", "o", "o"]])
+            expect(game.check_for_tie).to eql(false)
+        end
+    end
+
+    context "#handle_input" do
+        it "raises a BadColumnError if given anything other than a number from 1 to 7" do
+            expect{ game.handle_input("a") }.to raise_error(ConnectFour::BadColumnError)
+        end
+
+        it "returns the given number (1-7) minus 1" do
+            expect(game.handle_input("1")).to eql(0)
         end
     end
 end
